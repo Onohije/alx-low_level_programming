02e9316f0781c1ea4e682330f387572fc62655d9
Onohije <igbadumeonoh@gmail.com>
2023-02-17 17:36:39 -0800

    print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabets in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha--)
+		putchar(char);
+
+	putchar('\n');
+
+	return (0);
+}
