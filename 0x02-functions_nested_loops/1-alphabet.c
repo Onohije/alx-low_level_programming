1b5df2bff81e62a16ab7f8266ab73d6a715864c6
Onohije <igbadumeonoh@gmail.com>
2023-02-21 05:13:50 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * print_alphabet - prints alphabet in small letter
+ * Return: Always 0 (succes)
+ */
+
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
