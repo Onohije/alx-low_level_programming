800d141c0a0987c3f594f87f89609bd4efb90b28
Onohije <igbadumeonoh@gmail.com>
2023-02-21 11:16:46 -0800

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lower case
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char j;
+
+	while (i < 10)
+	{
+		j = 'a';
+		while (a < 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar(i);
+		i++;
+	}
+}
