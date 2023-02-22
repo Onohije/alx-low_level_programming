b764f30a725cc9d0a301cd110f0ff937415bae62
Onohije <igbadumeonoh@gmail.com>
2023-02-21 16:20:58 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet - print lowercase
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int a = 1;
+	char b;
+
+	while (a <= 10)
+	{
+		b = 'a';
+		while (b <= 'z')
+		{
+		_putchar(b);
+		b++;
+		}
+		_putchar('\n');
+		a++;
+	}
+}
+
