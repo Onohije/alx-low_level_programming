bad6eb99ac2665e96503960874fefdd822e4beb0
Onohije <igbadumeonoh@gmail.com>
2023-03-11 04:30:48 -0800

    static library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,12 +1,14 @@
 #include "main.h"
-#include <stdio.h>
+
+#include<stdio.h>
+void _puts(char *str);
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-void _puts(char *str)
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return ;
+	_puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
